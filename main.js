d26f3604f5b3743e713b0130d731a63f07b75d37
MDimas27 <mdimas.umb@gmail.com>
2022-06-14 17:47:12 +0700

    Javascript Basic - quiz function

--- main.js
+++ main.js
@@ -1,21 +1,8 @@
-const myMap = new Map([
-    ['1', 'a String key'],
-    [1, 'a number key'],
-    [true, true]
-]);
+const capital = {
+    "Jakarta": "Indonesia",
+    "London": "England",
+    "Tokyo": "Japan"
+}
+capital["New Delhi"] = "Indonesia";
 
-console.log(myMap);
-
-const capital = new Map([
-    ["Jakarta", "Indonesia"],
-    ["London", "England"],
-    ["Tokyo", "Japan"]
-]);
-
-console.log(capital);
-console.log(capital.size)
-console.log(capital.get("Tokyo"))
-console.log(capital.set("Nusantara", "Indonesia"))
-console.log(capital.size)
-
-
+console.log(capital["Indonesia"]);
